8437d9c2fa71e617b0b6761a3594cdd31f071f13
danhyal <farikoskillz2@gmail.com>
2019-12-12 17:21:22 +0000

    commented code

--- app/src/main/java/com/showcase/btecshowcase/MainActivity.java
+++ app/src/main/java/com/showcase/btecshowcase/MainActivity.java
@@ -24,6 +24,7 @@ import org.bytedeco.opencv.global.opencv_imgcodecs;
 import org.bytedeco.opencv.opencv_core.Mat;
 import org.bytedeco.opencv.opencv_core.Point;
 import org.bytedeco.opencv.opencv_core.Scalar;
+import org.bytedeco.opencv.opencv_core.Size;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -61,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        jsonresult();
+
     }
 
     @Override
@@ -72,9 +73,13 @@ public class MainActivity extends AppCompatActivity {
             case 7:
 
                 if(resultCode==RESULT_OK){
+                    try {
+                        String ImagePath = data.getData().getPath();
+                        Bitmap bmp=jsonresult(ImagePath);
 
-                    String ImagePath = data.getData().getPath();
-                    jsonresult(ImagePath);
+                    }catch (NullPointerException e){e.printStackTrace();}
+                    catch (ParseException f){f.printStackTrace();}
+
 
                 }
                 break;
@@ -83,66 +88,77 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public  void jsonresult(String ImagePath)  {
+    public  Bitmap jsonresult(String ImagePath) throws ParseException {
+        // retrieve and convert bitmap to opencv matrix
         Bitmap bitmap= BitmapFactory.decodeFile(ImagePath);
         Frame tempframe=convert1.convert(bitmap);
-        Mat matrix=converterToMat.convert(tempframe);
-        System.out.print(matrix.depth());
+        Mat img=converterToMat.convert(tempframe);
+        System.out.print(img.depth());
+        // init json objects
+        Gson gson = new Gson();
+        org.json.simple.JSONObject json=new org.json.simple.JSONObject();
+        org.json.simple.JSONArray throwawayarray=new org.json.simple.JSONArray();
+        JSONObject b64=new JSONObject();
+        JSONParser jsonParser=new JSONParser();
 
-//        Gson gson = new Gson();
-//        org.bytedeco.opencv.opencv_core.Mat img = org.bytedeco.opencv.global.opencv_imgcodecs.imread("/home/danhyal/download.jpeg");
-////        org.bytedeco.opencv.global.opencv_imgproc.resize(img,img,new Size(1200,1200));
-//
-//        ByteBuffer temp=img.getByteBuffer();
-//        byte[] arr = new byte[temp.remaining()];
-//        temp.get(arr);
-//        opencv_imgcodecs.imencode(".jpg", img, arr);
-//        String encoded= Base64.encodeBase64String(arr);
-//
-//        org.json.simple.JSONObject json=new org.json.simple.JSONObject();
-//        org.json.simple.JSONArray oof=new org.json.simple.JSONArray();
-//        JSONObject b64=new JSONObject();
-//        b64.put("b64",encoded);
-//        oof.add(b64);
-//        json.put("instances",oof);
-//        String server_url = "http://92.233.63.88:8501/v1/models/resnet_openimages:predict";
-//        JSONParser jsonParser=new JSONParser();
-//
-//        final long startTime = System.currentTimeMillis();
-//        String response = Requests.post(server_url).acceptCompress(true)
-//                .jsonBody(json).socksTimeout(10000)
-//                .send().readToText();
-//        Object obj=jsonParser.parse(response);
-//        JSONObject jobj=(JSONObject) obj;
-//        JSONArray content=(JSONArray) jobj.get("predictions");
-//        Iterator i = content.iterator();
-//
-//        JSONObject predictions = (JSONObject) i.next();
-//        int num_detections = ((Double) predictions.get("num_detections")).intValue();
-//        double[] detection_classes = gson.fromJson(predictions.get("detection_classes").toString(),(Type)double[].class);
-//        List<Double> detection_scores=new ArrayList<>();
-//        double[] detection_scoress =gson.fromJson(predictions.get("detection_scores").toString(),(Type)double[].class);
-//        for (double x:detection_scoress){
-//            if (x!=0.0f){
-//                detection_scores.add(x);
-//            }
-//        }
-//        Double[][] detection_boxess=gson.fromJson(predictions.get("detection_boxes").toString(), (Type) Double[][].class);
-//        List<List<Double>> detection_boxes= StreamEx.of(detection_boxess).map(a -> DoubleStreamEx.of(a).boxed().toList()).toList();
-//        for (int j=0;j<num_detections;j+=1){
-//            double confidance=detection_scores.get(j);
-//            if (confidance>0.7){
-//                int top= (int) (detection_boxes.get(j).get(0)*img.rows());
-//                int left=(int)(detection_boxes.get(j).get(1)*img.cols());
-//                int bottom=(int)(detection_boxes.get(j).get(2)*img.rows());
-//                int right=(int)(detection_boxes.get(j).get(3)*img.cols());
-//                org.bytedeco.opencv.global.opencv_imgproc.rectangle(img,new Point(left,top),new Point(right,bottom), Scalar.GREEN);
-//                //org.bytedeco.opencv.global.opencv_imgproc.putText(img, TensorCocoClasses[(int) detection_classes[j]],new Point(left,top),1,1,Scalar.RED);
-//
-//
-//            }
-//        }
-//        final long endTime = System.currentTimeMillis();
-//        return img;
+        // resize matrix to match warmup data
+        org.bytedeco.opencv.global.opencv_imgproc.resize(img,img,new Size(1920,1080));
+        // convert matrix into b64 encoded jpeg
+        ByteBuffer temp=img.getByteBuffer();
+        byte[] arr = new byte[temp.remaining()];
+        temp.get(arr);
+        opencv_imgcodecs.imencode(".jpg", img, arr);
+        String encoded= Base64.encodeBase64String(arr);
+
+        // add b64 string to json object and prep for transmission
+        b64.put("b64",encoded);
+        throwawayarray.add(b64);
+        json.put("instances",throwawayarray);
+        String server_url = "http://92.233.63.88:8501/v1/models/resnet_openimages:predict";
+
+        final long startTime = System.currentTimeMillis();
+        // send post request to remote server
+        String response = Requests.post(server_url).acceptCompress(true)
+                .jsonBody(json).socksTimeout(10000)
+                .send().readToText();
+        // parse response into json
+        Object obj=jsonParser.parse(response);
+        JSONObject jobj=(JSONObject) obj;
+        JSONArray content=(JSONArray) jobj.get("predictions");
+        Iterator i = content.iterator();
+
+
+        // filter json and convert into correct datatypes.
+        JSONObject predictions = (JSONObject) i.next();
+
+        int num_detections = ((Double) predictions.get("num_detections")).intValue();
+        double[] detection_classes = gson.fromJson(predictions.get("detection_classes").toString(),(Type)double[].class);
+        List<Double> detection_scores=new ArrayList<>();
+        double[] detection_scoress =gson.fromJson(predictions.get("detection_scores").toString(),(Type)double[].class);
+        for (double x:detection_scoress){
+            if (x!=0.0f){
+                detection_scores.add(x);
+            }
+        }
+        Double[][] detection_boxess=gson.fromJson(predictions.get("detection_boxes").toString(), (Type) Double[][].class);
+        List<List<Double>> detection_boxes= StreamEx.of(detection_boxess).map(a -> DoubleStreamEx.of(a).boxed().toList()).toList();
+        // interate over the number of detections to draw rectangles and text for each detection on matrix.
+        for (int j=0;j<num_detections;j+=1){
+            double confidance=detection_scores.get(j);
+            if (confidance>0.7){
+                int top= (int) (detection_boxes.get(j).get(0)*img.rows());
+                int left=(int)(detection_boxes.get(j).get(1)*img.cols());
+                int bottom=(int)(detection_boxes.get(j).get(2)*img.rows());
+                int right=(int)(detection_boxes.get(j).get(3)*img.cols());
+                org.bytedeco.opencv.global.opencv_imgproc.rectangle(img,new Point(left,top),new Point(right,bottom), Scalar.GREEN);
+                //org.bytedeco.opencv.global.opencv_imgproc.putText(img, TensorCocoClasses[(int) detection_classes[j]],new Point(left,top),1,1,Scalar.RED);
+
+
+            }
+        }
+        final long endTime = System.currentTimeMillis();
+        System.out.print((endTime-startTime)/1000);
+        // temp
+        return bitmap;
     }
 }
