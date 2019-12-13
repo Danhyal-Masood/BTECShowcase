498e5229a332b4cb5e752b2128a2014f509f493e
danhyal <danhyal@gmail.com>
2019-12-13 23:26:41 +0000

    Fixed networking and added bitmap to imageview

--- app/src/main/java/com/showcase/btecshowcase/MainActivity.java
+++ app/src/main/java/com/showcase/btecshowcase/MainActivity.java
@@ -6,19 +6,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
 
-import net.dongliu.requests.Requests;
 
 import org.apache.commons.codec.binary.Base64;
 import org.bytedeco.javacv.AndroidFrameConverter;
 import org.bytedeco.javacv.Frame;
+import org.bytedeco.javacv.FrameFilter;
 import org.bytedeco.javacv.OpenCVFrameConverter;
 import org.bytedeco.opencv.global.opencv_imgcodecs;
 import org.bytedeco.opencv.opencv_core.Mat;
@@ -31,12 +35,20 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.lang.reflect.Type;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 import one.util.streamex.DoubleStreamEx;
 import one.util.streamex.StreamEx;
 
@@ -76,11 +88,17 @@ public class MainActivity extends AppCompatActivity {
 
                 if(resultCode==RESULT_OK){
                     try {
-                        String ImagePath = data.getData().getPath();
-                        Bitmap bmp=jsonresult(ImagePath);
+                        ImageView v1=findViewById(R.id.imageView);
+                        Uri ImageUri = data.getData();
+                        Bitmap bitmap= MediaStore.Images.Media.getBitmap(this.getContentResolver(), ImageUri);
+                        NetworkTask nt=new NetworkTask();
+
+                        Bitmap bmp=nt.execute(bitmap).get();
+                        v1.setImageBitmap(bmp);
+                        Log.d(TAG,String.valueOf(bmp.getHeight()));
 
                     }catch (NullPointerException e){e.printStackTrace();}
-                    catch (ParseException f){f.printStackTrace();}
+                    catch (Exception e){e.printStackTrace();}
 
 
                 }
@@ -90,12 +108,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public  Bitmap jsonresult(String ImagePath) throws ParseException {
-        // retrieve and convert bitmap to opencv matrix
-        Bitmap bitmap= BitmapFactory.decodeFile(ImagePath);
+
+    public class NetworkTask extends AsyncTask<Bitmap, Void, Bitmap> {
+
+    @Override
+    public Bitmap doInBackground(Bitmap... bitmaps) {
+        Bitmap bitmap=bitmaps[0];
+
         Frame tempframe=convert1.convert(bitmap);
         Mat img=converterToMat.convert(tempframe);
-        System.out.print(img.depth());
+        Log.d(TAG,String.valueOf(img.arrayHeight()));
         // init json objects
         Gson gson = new Gson();
         org.json.simple.JSONObject json=new org.json.simple.JSONObject();
@@ -112,17 +134,21 @@ public class MainActivity extends AppCompatActivity {
         opencv_imgcodecs.imencode(".jpg", img, arr);
         String encoded= Base64.encodeBase64String(arr);
 
+
         // add b64 string to json object and prep for transmission
         b64.put("b64",encoded);
         throwawayarray.add(b64);
         json.put("instances",throwawayarray);
-        String server_url = "http://92.233.63.88:8501/v1/models/resnet_openimages:predict";
+        String server_url = "http://92.233.63.88:8501/v1/models/coconet:predict";
 
         final long startTime = System.currentTimeMillis();
-        // send post request to remote server
-        String response = Requests.post(server_url).acceptCompress(true)
-                .jsonBody(json).socksTimeout(10000)
-                .send().readToText();
+        OkHttpClient client = new OkHttpClient();
+        final MediaType JSON= MediaType.get("application/json; charset=utf-8");
+        RequestBody requestBody=RequestBody.create(json.toJSONString(),JSON);
+        Request request=new Request.Builder().url(server_url).post(requestBody).build();
+        try {
+             String response=client.newCall(request).execute().body().string();
+
         // parse response into json
         Object obj=jsonParser.parse(response);
         JSONObject jobj=(JSONObject) obj;
@@ -162,5 +188,14 @@ public class MainActivity extends AppCompatActivity {
         System.out.print((endTime-startTime)/1000);
         // temp
         return bitmap;
+        }catch (ParseException e){}
+       catch (IOException e){}
+        return bitmap;
     }
+
+    @Override
+    protected void onPostExecute(Bitmap result) {
+
+    }
+}
 }
